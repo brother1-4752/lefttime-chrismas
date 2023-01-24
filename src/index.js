4279178bf0caf14d1ab6770f88a24cee5a9ffab7
brother1-4752 <60454376+brother1-4752@users.noreply.github.com>
2023-01-24 17:11:15 +0900

    update component for presenting time until chrismas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const clockTitle = document.querySelector(".js-clock");
-const chrismas = new Date(2022, 11, 25); //크리스마스 : 23/12/25
-const current = new Date();
-const current_year = current.getFullYear();
-const current_month = current.getMonth();
-const current_date = current.getDate();
-const current_hour = current.getHours();
-const current_minute = current.getMinutes();
-const current_second = current.getSeconds();
+let untilChrismas;
 
-console.dir(current - chrismas);
+function timeUntilChristmas() {
+  let currentDate = new Date();
+  let christmas = new Date(currentDate.getFullYear(), 11, 25);
+  let difference = christmas.getTime() - currentDate.getTime();
+  let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(
+    (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+  clockTitle.innerHTML = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds until Christmas!`;
+}
+
+untilChrismas = setInterval(timeUntilChristmas, 1000);
